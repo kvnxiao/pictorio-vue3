13fb9137bb1a47982539c7c6981718448934116a
Kevin Xiao <xlzqwerty1@gmail.com>
2020-10-24 20:14:29 -0400

    Apply formatting

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,5 +1,7 @@
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable prettier/prettier */
+declare module "*.vue" {
+  import type { DefineComponent } from "vue"
   const component: DefineComponent<{}, {}, any>
   export default component
 }
