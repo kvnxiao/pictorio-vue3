e593a1611243522f8d3ef108f2c8ab02359f1da3
Kevin Xiao <xlzqwerty1@gmail.com>
2020-10-24 22:26:55 -0400

    Add chat component

---
+++ src/components/game/Chat.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="chat">
+    <div class="box">
+      <div class="filler">
+        <div class="conversation">
+          <ul>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+            <li>test</li>
+          </ul>
+        </div>
+
+        <div class="chatbox field has-addons">
+          <div class="control" id="chat-input">
+            <input type="text" class="input" placeholder="Enter your guess" />
+          </div>
+          <div class="control">
+            <button class="button">
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name: "Chat",
+})
+</script>
+
+<style lang="sass" scoped>
+#chat
+  .filler
+    display: flex
+    flex-direction: column
+
+  #chat-input
+    width: 100%
+
+.conversation
+  overflow-y: scroll
+  flex-grow: 1
+  li
+    text-align: left
+    padding: 0 0.25rem
+</style>
--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="game">
+  <div id="game">
     <div class="grid">
       <player-info />
       <template v-if="isGameStarted">
@@ -8,13 +8,14 @@
       <template v-else>
         <waiting />
       </template>
-      <div></div>
+      <chat />
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, Ref, ref } from "vue"
+import Chat from "@/components/game/Chat.vue"
 import Drawing from "@/components/game/Drawing.vue"
 import Waiting from "@/components/game/Waiting.vue"
 import PlayerInfo from "@/components/game/PlayerInfo.vue"
@@ -25,6 +26,7 @@ export default defineComponent({
     Drawing,
     Waiting,
     PlayerInfo,
+    Chat,
   },
   setup() {
     const isGameStarted: Ref<boolean> = ref(true)
@@ -36,31 +38,31 @@ export default defineComponent({
 })
 </script>
 
-<style lang="sass" scoped>
-.game
+<style lang="sass">
+#game
   display: flex
   width: 100%
   height: calc(100% - 52px)
   justify-content: center
 
-.grid
-  width: 100%
-  padding: 2rem
-  margin: auto
-  display: grid
-  grid-template-columns: 15% auto 15%
-  grid-column-gap: 1rem
+  .grid
+    width: 100%
+    padding: 2rem
+    margin: auto
+    display: grid
+    grid-template-columns: 15% auto 15%
+    grid-column-gap: 1rem
 
-.box
-  padding: 0.25rem
-  width: 100%
-  height: 100%
-  background: #dadfdb
-  box-shadow: 0 0.25em 0.5em rgba(10, 10, 10, 0.15), 0 0px 2px 1px rgba(10, 10, 10, 0.05)
+  .box
+    padding: 0.25rem
+    width: 100%
+    height: 100%
+    background: #dadfdb
+    box-shadow: 0 0.25em 0.5em rgba(10, 10, 10, 0.15), 0 0px 2px 1px rgba(10, 10, 10, 0.05)
 
-.filler
-  border-radius: 3px
-  background: white
-  width: 100%
-  height: 100%
+  .filler
+    border-radius: 3px
+    background: white
+    width: 100%
+    height: 100%
 </style>
