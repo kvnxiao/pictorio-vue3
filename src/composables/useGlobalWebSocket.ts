f00dad119e251afdbe7723e075a08e360c0d1747
Kevin Xiao <xlzqwerty1@gmail.com>
2020-10-29 02:27:09 -0400

    Add global websocket composition

---
+++ src/composables/useGlobalWebSocket.ts
@@ -0,0 +1,62 @@
+import { ComputedRef, Ref, computed, ref } from "vue"
+
+export enum WebSocketState {
+  CONNECTING = 0,
+  OPEN = 1,
+  CLOSING = 2,
+  CLOSED = 3,
+}
+
+const READY_STATE_MAPPING = [
+  WebSocketState.CONNECTING,
+  WebSocketState.OPEN,
+  WebSocketState.CLOSING,
+  WebSocketState.CLOSED,
+]
+
+let ws: WebSocket | null = null
+
+export function useGlobalWebSocket() {
+  const data: Ref<unknown> = ref(null)
+  const error: Ref<Event | null> = ref(null)
+  const state: ComputedRef<WebSocketState> = computed(
+    () => READY_STATE_MAPPING[ws?.readyState ?? 0],
+  )
+
+  const close = (code?: number, reason?: string) => {
+    ws?.close(code, reason)
+  }
+
+  const send = (data: string | ArrayBufferLike | Blob | ArrayBufferView) => {
+    ws?.send(data)
+  }
+
+  const connect = (
+    url: string,
+    onOpen?: (event: Event) => void,
+    onClose?: (event: CloseEvent) => void,
+  ) => {
+    if (ws) {
+      if (state.value !== WebSocketState.CLOSED) {
+        return
+      }
+      ws = null
+    }
+
+    ws = new WebSocket(url)
+    if (onOpen) ws.onopen = onOpen
+    if (onClose) ws.onclose = onClose
+
+    ws.onerror = (event: Event) => {
+      error.value = event
+    }
+  }
+
+  return {
+    data,
+    state,
+    close,
+    send,
+    connect,
+  }
+}
