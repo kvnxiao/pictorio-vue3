cb8f1be1ac414b1062f276c23b55a8e8f214c45a
Kevin Xiao <xlzqwerty1@gmail.com>
2020-12-19 01:13:46 -0500

    Style loading screen

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -4,7 +4,11 @@
       <Room />
     </template>
     <template #fallback>
-      <div class="flex w-full h-full items-center justify-center">Loading...</div>
+      <div class="flex w-full h-full items-center justify-center">
+        <p class="bg-white py-6 px-10 shadow-md rounded-md text-xl text-gray-700">
+          Loading...
+        </p>
+      </div>
     </template>
   </Suspense>
 </template>
