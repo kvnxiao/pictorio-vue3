0a4b63da29609cdeccad4085a1619dd5857c1225
Kevin Xiao <xlzqwerty1@gmail.com>
2020-12-27 16:43:19 -0500

    Add spectator join message

--- src/components/game/panels/PanelCenter.vue
+++ src/components/game/panels/PanelCenter.vue
@@ -19,11 +19,15 @@
 
 <script lang="ts">
 import { GameStatus, TurnStatus } from "@/models/status"
-import { PropType, defineComponent } from "vue"
+import { PropType, defineComponent, watch } from "vue"
 import Drawing from "@/components/game/panels/center/Drawing.vue"
 import GameOver from "@/components/game/panels/center/GameOver.vue"
+import { PlayerState } from "@/models/playerState"
+import { ToastMessageMutations } from "@/store/toastMsgStore/mutations"
 import { User } from "@/models/user"
 import Waiting from "@/components/game/panels/center/Waiting.vue"
+import { useToastMsgStore } from "@/store/toastMsgStore"
+import { useUserStore } from "@/store/userStore"
 
 export default defineComponent({
   name: "PanelCenter",
@@ -63,7 +67,22 @@ export default defineComponent({
       default: 0,
     },
   },
-  setup() {
+  setup(props) {
+    const userStore = useUserStore()
+    const toastMsgStore = useToastMsgStore()
+
+    watch(
+      () => userStore.state.playerStates[props.selfUser.id],
+      (p: PlayerState | undefined) => {
+        if (p && p.isSpectator) {
+          toastMsgStore.commit(ToastMessageMutations.SET, {
+            message: "You've joined as a spectator!",
+            type: "warning",
+          })
+        }
+      },
+    )
+
     return {
       GameStatus,
       TurnStatus,
